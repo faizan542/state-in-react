c4efd472e23b1e150bb9eb41c5c0f6a6fab89ac1
faizan542 <faizansilat100@gmail.com>
2024-03-02 18:29:01 +0500

    State in Js

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
+import { useState } from 'react'
+import './App.css'
 
 function App() {
+  const [count, setCount] = useState(0);
 
   return (
     <>
-
+      <div className='count'>
+        <p>The count is {count} </p>
+        <div className='btn'>
+          <button onClick={() => { setCount(count + 1) }}>Count</button>
+        </div>
+      </div>
     </>
   )
 }
